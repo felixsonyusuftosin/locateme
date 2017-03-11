c9b5f4742cf510715dc12b5bcbee7af17a6570bf
Tosin Felixson Yusuf <Tosin Felixson Yusuf>
2017-03-11 19:24:20 +0100

    firstfull

---
+++ js/index.js
@@ -0,0 +1,29 @@
+var previous;
+var next;
+
+$(document).ready(function(){	
+	callmap();		
+});
+
+function callmap(){
+    $(document).on('click','.next',function(){
+    var item = $(this).attr('data-item');
+    previous = '#'+$(this).parents().find('.bd').attr('id');
+      $(this).parents().find('.bd').hide('fade',100,function(){
+          $(this).css('display', 'none');
+          $('#'+item).show('slide',{'direction':'right'},300, function(){
+               next = '#'+item;
+          })
+      })
+    });
+    $(document).on('click','.previous',function(){
+    console.log(next); 
+      console.log(previous); 
+         $(next).hide('fade',100,function(){
+             $(previous).show('slide',{'direction':'right'},200, function(){ 
+                 $(this).css('display', 'block');            
+          });
+         })
+    });
+    
+}
